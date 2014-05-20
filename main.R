9249c8baa71978d4543300462347ab455d8ba606
Linda Zhu <linda.yue.zhu@googlemail.com>
2014-05-20 16:09:25 +0200

    main.R

---
+++ main.R
@@ -0,0 +1,14 @@
+source("R/test_relation.R")
+
+source("R/test_root_question.R")
+
+source("R/test_scales.R")
+
+source("R/test_root_question.R")
+
+source("R/test_unique.R")
+
+source("R/test_identifier.R")
+
+source("R/run_all_test.R")
+
